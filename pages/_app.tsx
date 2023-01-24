5ec63d40b4939d44a6abcfa17bc314e4ae4d4be3
Srdjan <srdjancoralic5@gmail.com>
2023-01-24 17:16:02 +0100

    add mui theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+
+import { lightTheme, darkTheme } from "../themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
