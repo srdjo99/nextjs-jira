5e9757b1a5e9d6f25b556cea53750cb0faaf8e4c
Srdjan <srdjancoralic5@gmail.com>
2023-01-27 21:23:32 +0100

    add basic cards layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 
 import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>
-      <Typography variant='h1' color='primary'>
-        Hola Mundo
-      </Typography>
+    <Layout title='Home - Jira'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Pending' />
+
+            <CardContent>
+              {/* Add new entry */}
+              {/* List of new entries */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='In progress' />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title='Completed' />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 };
