84ab986e9a1a46629722e0798900b1da88c9dcbe
Srdjan <srdjancoralic5@gmail.com>
2023-01-28 10:07:09 +0100

    add states and handlers to form

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,34 +1,67 @@
+import { ChangeEvent, useState } from 'react';
 import { Box, Button, TextField } from '@mui/material';
 
 import { AddCircleOutlineOutlined, SaveOutlined } from '@mui/icons-material';
 
 export const NewEntry = () => {
+  const [isAdding, setIsAdding] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [touched, setTouched] = useState(false);
+
+  const onTextFieldChanged = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInputValue(e.target.value);
+  };
+
+  const onSave = () => {
+    if (inputValue.length === 0) return;
+
+    console.log({ inputValue });
+  };
+
   return (
     <Box sx={{ marginBottom: 2, paddingX: 2 }}>
-      <Button
-        startIcon={<AddCircleOutlineOutlined />}
-        fullWidth
-        variant='outlined'
-      >
-        Add Task
-      </Button>
+      {isAdding ? (
+        <>
+          <TextField
+            fullWidth
+            sx={{ marginTop: 2, marginBottom: 1 }}
+            placeholder='New entry'
+            autoFocus
+            multiline
+            label='New entry'
+            helperText={inputValue.length <= 0 && touched && 'Enter a value'}
+            error={inputValue.length <= 0 && touched}
+            value={inputValue}
+            onChange={onTextFieldChanged}
+            onBlur={() => setTouched(true)}
+          />
 
-      <TextField
-        fullWidth
-        sx={{ marginTop: 2, marginBottom: 1 }}
-        placeholder='New entry'
-        autoFocus
-        multiline
-        label='New entry'
-        helperText='Enter a value'
-      />
-
-      <Box display='flex' justifyContent='space-between'>
-        <Button variant='text'>Cancel</Button>
-        <Button variant='outlined' color='secondary' endIcon={<SaveOutlined />}>
-          Save
+          <Box display='flex' justifyContent='space-between'>
+            <Button variant='text' onClick={() => setIsAdding(false)}>
+              Cancel
+            </Button>
+            <Button
+              variant='outlined'
+              color='secondary'
+              endIcon={<SaveOutlined />}
+              onClick={onSave}
+            >
+              Save
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <Button
+          startIcon={<AddCircleOutlineOutlined />}
+          fullWidth
+          variant='outlined'
+          onClick={() => setIsAdding(true)}
+        >
+          Add Task
         </Button>
-      </Box>
+      )}
     </Box>
   );
 };
